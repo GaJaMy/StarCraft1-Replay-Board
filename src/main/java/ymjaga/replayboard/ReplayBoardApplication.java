5f9a1d1778a0a7656530f4cec5dee05580f61c09
HyungDong <mansa0805@gmail.com>
2024-02-15 22:09:38 +0900

    Update README.md, 초êinit project

---
+++ src/main/java/ymjaga/replayboard/ReplayBoardApplication.java
@@ -0,0 +1,13 @@
+package ymjaga.replayboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplayBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplayBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ymjaga/replayboard/ReplayBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package ymjaga.replayboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplayBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
